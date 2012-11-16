3de498d5abce91e456186f0173bdc6053b5135e9
Milan Divilek <milan.divilek@magnolia-cms.com>
2012-11-16 11:14:13 +0100

    MAGNOLIA-4667 removed magnoliaPublic webapp from bundle, added script to magnolia_control script files which create the public webapp during first run

--- magnolia-tomcat-bundle/src/release/tomcat/bin/magnolia_control.bat
+++ magnolia-tomcat-bundle/src/release/tomcat/bin/magnolia_control.bat
@@ -4,12 +4,19 @@ if ""%1"" == ""stop"" goto doStop
 goto noCommand
 
 :doStart
+if not exist .installed if not exist ..\webapps\magnoliaPublic goto doInstall
 call startup.bat
 goto end
 
 :doStop
 call shutdown.bat
 goto end
+
+:doInstall
+echo First run -> create magnoliaPublic webapp from magnoliaAuthor webapp.
+xcopy ..\webapps\magnoliaAuthor ..\webapps\magnoliaPublic /e /i /h
+echo This file indicates that the public webapp was created. The file is created during first run. > .installed
+goto doStart
 
 :noCommand
 echo Please provide "start" or "stop" as argument.
--- magnolia-tomcat-bundle/src/release/tomcat/bin/magnolia_control.sh
+++ magnolia-tomcat-bundle/src/release/tomcat/bin/magnolia_control.sh
@@ -16,6 +16,12 @@ PRGDIR=`dirname "$PRG"`
 #---
 
 if [ "$1" = "start" ] ; then
+    # create public webapp when "installed" file and "magnoiaPublic" directory doesn't exist
+    if [ ! -e "$PRGDIR/.installed" ] && [ ! -d "$PRGDIR/../webapps/magnoliaPublic" ] ; then
+      echo "First run -> create magnoliaPublic webapp from magnoliaAuthor webapp."
+      cp -a $PRGDIR/../webapps/magnoliaAuthor $PRGDIR/../webapps/magnoliaPublic
+      echo "This file indicates that the public webapp was created. The file is created during first run." > $PRGDIR/.installed
+    fi
     # to start with jpda (debugging):
     # exec "$PRGDIR"/catalina.sh jpda start
     exec "$PRGDIR"/startup.sh
